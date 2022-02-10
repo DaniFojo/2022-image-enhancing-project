d9e4429fad485acbe32d93dffe2d0bd62e3dfd49
plopez <plopez@clevertask.com>
2022-02-10 19:09:12 +0100

    changes in Decom convolutional layers

--- model/model.py
+++ model/model.py
@@ -10,52 +10,22 @@ class DecomNet(nn.Module):
 	Constraint: images share the same reflectance.
 	"""
 
-	def __init__(self, out_channels=64, krnl_size=3):
+	def __init__(self, out_channels=64, kernel_size=3):
 		super().__init__()
-		
-
-		# pytorch:	
-		# 	self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-
-		# tensorflow:
-		# en el código de Retinex:
-		# 	conv = tf.layers.conv2d(input_im, channel, kernel_size * 3, padding='same',...)
-		# Ejemplo en stackoverflow:
-		#	Convolution Layer with 32 filters and a kernel size of 5
-		#	conv1 = tf.layers.conv2d(x, 32, 5, activation=tf.nn.relu) 
-		# tf.layers.conv2d: 
-		# 	filters: Integer, the dimensionality of the output space (i.e. the number of filters in the convolution).
-		#   (out channels)
-		# https://www.tutorialexample.com/understand-tf-layers-conv2d-with-examples-tensorflow-tutorial/
-		# aparentemente obtiene el in_channels de la propia imagen, asumiendo que es de la forma:
-		#    [batch, in_height, in_width, in_channels]
-
-		# Tensorflow Padding:
-		# https://wandb.ai/krishamehta/seo/reports/Difference-Between-SAME-and-VALID-Padding-in-TensorFlow--VmlldzoxODkwMzE
-		# When padding == ”VALID”, the input image is not padded.
-		# When padding == “SAME”, the input is half padded. 
-		# The padding type is called SAME because the output size is the same as the input size (when stride=1). 
-		# Using ‘SAME’ ensures that the filter is applied to all the elements of the input. 
-
-		# En pytorch debo calcular el padding para obtener output size = input size:
-		# P = (K -1) / 2
-
-
 		# Convolutional with no activation function:
 		# Notar kernel size * 3.
-		kern = krnl_size * 3
-		padd = (kern -1) / 2
-		self.conv1 = nn.Conv2d(in_channels=3, out_channels=out_channels, kernel_size=kern, stride=1, padding=padd)  
+		padding = (kernel_size - 1) / 2
+		self.conv1 = nn.Conv2d(in_channels=3, out_channels=out_channels, kernel_size=kernel_size, stride=1, padding=padding)  
 
 		# 5 convolutional layers with a ReLU 
-		self.conv2 = nn.Conv2d(...)  # + relu
-		self.conv3 = nn.Conv2d(...)  # + relu
-		self.conv4 = nn.Conv2d(...)  # + relu
-		self.conv5 = nn.Conv2d(...)  # + relu
-		self.conv6 = nn.Conv2d(...)  # + relu
+		self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=1, padding=padding)
+		self.conv3 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=1, padding=padding)
+		self.conv4 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=1, padding=padding)
+		self.conv5 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=1, padding=padding)
+		self.conv6 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=1, padding=padding)
 
 		# Convolutional with no activation function:
-		self.conv7 = nn.Conv2d(...)  
+		self.conv7 = nn.Conv2d(in_channels=out_channels, out_channels=4, kernel_size=kernel_size, stride=1, padding=padding)
 
 	def forward(self, x):
 		x = self.conv1(x)
@@ -66,11 +36,11 @@ class DecomNet(nn.Module):
 		x = F.relu(self.conv6(x))
 		x = self.conv7(x)
 
-		# parte x en 2:
-		R = F.sigmoid(x[:,:,:,0:3]) # pseudocódigo sin revisar.
-		L = F.sigmoid(x[:,:,:,3:4]) # pseudocódigo sin revisar.
-
-    	return R, L
+		# Reflectance and illuminance
+		R = F.sigmoid(x[:,0:3,:,:]) # n x c x h x w
+		L = F.sigmoid(x[:,3:4,:,:]) # n x c x h x w
+		
+		return R, L
 
 
 
