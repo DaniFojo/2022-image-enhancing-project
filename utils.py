34fc0fd8601c1496445f8b1eb02e06f5f2fe7dcf
vcastillo8 <pedrapapertissora@gmail.com>
2022-03-24 15:49:39 +0100

    Denoise function added to utils

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import cv2
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
 # Aux functions
 
 def compute_accuracy(preds, labels):
@@ -5,3 +10,10 @@ def compute_accuracy(preds, labels):
     acc = pred_labels.eq(labels.view_as(pred_labels)).sum().item() / len(
         pred_labels)
     return acc
+
+def denoise(torch_image):
+    trans = transforms.ToPILImage()
+    cv2_image = np.array(trans(torch_image))
+    denoised_image = cv2.fastNlMeansDenoisingColored(cv2_image, None, 10, 10, 7, 21)
+    plt.imshow(denoised_image)
+    plt.show()
